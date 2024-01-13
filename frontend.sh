89aa023d87d9827c13094ade7a1badb5df84a552
kumar <kumardhayait15@gmail.com>
2024-01-13 12:40:40 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -6,3 +6,7 @@ systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip
