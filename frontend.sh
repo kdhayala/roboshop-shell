a588c30c819de19b42d0ab9a08f982a6c4dd88fb
kumar <kumardhayait15@gmail.com>
2024-01-13 13:29:16 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -5,8 +5,6 @@ systemctl start nginx
 
 rm -rf /usr/share/nginx/html/*
 
-curl -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
-
 unzip /tmp/frontend.zip
