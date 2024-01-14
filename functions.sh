4f023798fce76c9e13f3c8e1a85c12e214c375e1
kumar <kumardhayait15@gmail.com>
2024-01-14 15:39:28 +0530

    input functions

--- functions.sh
+++ functions.sh
@@ -12,11 +12,11 @@ echo  Functions EXIt status $?
 ## you can declare var in main program,you can access thst in funfunction in vice versa.
 # Function have it's own special variables
 
-input()
- echo first arguements - $1
- echo second arguements - $2
- echo  third arguements - $*
- echo  fourth arguements - $#
+input() {
+  echo first arguements - $1
+  echo second arguements - $2
+  echo  third arguements - $*
+  echo  fourth arguements - $#
 }
 
 input abc 1234
